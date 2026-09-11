d3eb04a105b8491e4749aea7bad34d9afcb761f6
MaskdDev <maskddev@gmail.com>
2026-09-11 23:38:45 +1000

    feat: Create reply struct and reply helpers on CommandContext

--- src/context/command.rs
+++ src/context/command.rs
@@ -1,10 +1,16 @@
 use crate::commands::{Command, TwilightCommand};
 use crate::context::AppContext;
+use crate::model::reply::CreateReply;
 use std::fmt::Debug;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use twilight_http::client::InteractionClient;
+use twilight_http::request::application::interaction::CreateFollowup;
+use twilight_http::{Error, Response};
 use twilight_model::application::interaction::Interaction;
 use twilight_model::application::interaction::application_command::CommandData;
+use twilight_model::channel::Message;
+use twilight_model::http::interaction::{InteractionResponse, InteractionResponseType};
 
 pub struct CommandContext<T, E>
 where
@@ -19,6 +25,9 @@ where
 
     /// The interaction that triggered this command, with its command data stripped out.
     pub interaction: Arc<Interaction>,
+
+    /// Whether an initial response has been made for this command.
+    has_responded: Arc<AtomicBool>,
 
     /// The locale of this interaction.
     pub locale: Option<String>,
@@ -37,6 +46,7 @@ where
             app_ctx: self.app_ctx.clone(),
             command: Arc::clone(&self.command),
             interaction: Arc::clone(&self.interaction),
+            has_responded: Arc::clone(&self.has_responded),
             command_data: Arc::clone(&self.command_data),
             locale: self.locale.clone(),
         }
@@ -61,6 +71,7 @@ where
             app_ctx: ctx,
             command: Arc::clone(&command.command),
             interaction: interaction.into(),
+            has_responded: Arc::new(AtomicBool::from(false)),
             command_data: command_data.into(),
             locale,
         }
@@ -68,5 +79,85 @@ where
     /// Get the interaction client from this context's HTTP client and application ID.
     pub fn interaction_client(&self) -> InteractionClient<'_> {
         self.app_ctx.interaction_client()
+    }
+
+    /// Send a reply in response to this command.
+    ///
+    /// Sends an initial response if no response to this command has been sent, or
+    /// edits an existing response if one has been made.
+    pub async fn send(&self, reply: CreateReply) -> Result<(), Error> {
+        // Check if a response has been made
+        if self
+            .has_responded
+            .compare_exchange(false, true, Ordering::Release, Ordering::Acquire)
+            .is_ok()
+        {
+            self.send_response(reply).await?;
+        } else {
+            self.edit_response(reply).await?;
+        }
+
+        // Return Ok
+        Ok(())
+    }
+
+    /// Send a reply in response to this command.
+    pub async fn send_response(&self, reply: CreateReply) -> Result<(), Error> {
+        // Create interaction response
+        let response = InteractionResponse {
+            kind: InteractionResponseType::ChannelMessageWithSource,
+            data: Some(reply.build()),
+        };
+
+        // Respond to interaction
+        self.interaction_client()
+            .create_response(self.interaction.id, &self.interaction.token, &response)
+            .await?;
+
+        // Return Ok
+        Ok(())
+    }
+
+    /// Update an initial interaction response.
+    pub async fn edit_response(&self, reply: CreateReply) -> Result<Response<Message>, Error> {
+        // Get interaction client
+        let client = self.interaction_client();
+
+        // Respond to interaction
+        let message = reply
+            .apply(client.update_response(&self.interaction.token))
+            .await?;
+
+        // Return message response
+        Ok(message)
+    }
+
+    /// Defer a response to this command.
+    ///
+    /// Does nothing if this command has already been responded to.
+    pub async fn defer(&self, ephemeral: bool) -> Result<(), Error> {
+        // Set the has responded flag to true, if not done already.
+        if self
+            .has_responded
+            .compare_exchange(false, true, Ordering::Release, Ordering::Acquire)
+            .is_err()
+        {
+            // Already responded to interaction, exit early.
+            return Ok(());
+        }
+
+        // Create interaction response
+        let response = InteractionResponse {
+            kind: InteractionResponseType::DeferredChannelMessageWithSource,
+            data: Some(CreateReply::new().ephemeral(ephemeral).build()),
+        };
+
+        // Respond to interaction
+        self.interaction_client()
+            .create_response(self.interaction.id, &self.interaction.token, &response)
+            .await?;
+
+        // Return Ok
+        Ok(())
     }
 }
---
+++ src/model/reply.rs
@@ -0,0 +1,246 @@
+use bitflags::bitflags;
+use twilight_http::request::application::interaction::UpdateResponse;
+use twilight_model::channel::message::{AllowedMentions, Component, Embed, MessageFlags};
+use twilight_model::http::attachment::Attachment;
+use twilight_model::http::interaction::InteractionResponseData;
+use twilight_model::poll::Poll;
+
+/// A reply to be sent in response to a command interaction.
+#[derive(Default)]
+pub struct CreateReply {
+    data: InteractionResponseData,
+    clear_flags: ClearFlags,
+}
+
+/// Builder helper methods, similar to [`twilight_util::builder::InteractionResponseDataBuilder`].
+impl CreateReply {
+    /// Create a new empty reply.
+    pub const fn new() -> Self {
+        Self {
+            data: InteractionResponseData {
+                allowed_mentions: None,
+                attachments: None,
+                choices: None,
+                components: None,
+                content: None,
+                custom_id: None,
+                embeds: None,
+                flags: None,
+                title: None,
+                tts: None,
+                poll: None,
+            },
+            clear_flags: ClearFlags::empty(),
+        }
+    }
+
+    /// Build the reply, returning an [`InteractionResponseData`] struct.
+    pub fn build(self) -> InteractionResponseData {
+        self.data
+    }
+
+    /// Apply this reply as an update onto an [`UpdateResponse`].
+    pub fn apply<'a>(&'a self, mut update_response: UpdateResponse<'a>) -> UpdateResponse<'a> {
+        // Add content to update, if present.
+        update_response = match &self.data.content {
+            Some(content) => update_response.content(Some(content)),
+            None if self.clear_flags.contains(ClearFlags::CONTENT) => update_response.content(None),
+            None => update_response,
+        };
+
+        // Add embeds to update, if present.
+        update_response = match &self.data.embeds {
+            Some(embeds) => update_response.embeds(Some(embeds)),
+            None if self.clear_flags.contains(ClearFlags::EMBEDS) => update_response.embeds(None),
+            None => update_response,
+        };
+
+        // Add components to update, if present.
+        update_response = match &self.data.components {
+            Some(components) => update_response.components(Some(components)),
+            None if self.clear_flags.contains(ClearFlags::COMPONENTS) => {
+                update_response.components(None)
+            }
+            None => update_response,
+        };
+
+        // Add allowed mentions to update, if present.
+        update_response = match &self.data.allowed_mentions {
+            Some(allowed_mentions) => update_response.allowed_mentions(Some(allowed_mentions)),
+            None if self.clear_flags.contains(ClearFlags::MENTIONS) => {
+                update_response.allowed_mentions(None)
+            }
+            None => update_response,
+        };
+
+        // Add flags and attachments to update, if present.
+        if let Some(flags) = self.data.flags {
+            update_response = update_response.flags(flags)
+        }
+        if let Some(attachments) = &self.data.attachments {
+            update_response = update_response.attachments(attachments)
+        }
+
+        // Return update response
+        update_response
+    }
+
+    /// Set the [`AllowedMentions`] of the reply.
+    ///
+    /// Defaults to [`None`].
+    pub fn allowed_mentions(mut self, allowed_mentions: AllowedMentions) -> Self {
+        self.data.allowed_mentions = Some(allowed_mentions);
+
+        self
+    }
+
+    /// Set this reply to clear the response's [`AllowedMentions`]. Only takes effect if this
+    /// reply is being used to modify an existing response.
+    ///
+    /// This is undone if allowed mentions are set following this call.
+    pub fn clear_allowed_mentions(mut self) -> Self {
+        self.data.allowed_mentions = None;
+        self.clear_flags.insert(ClearFlags::MENTIONS);
+        self
+    }
+
+    /// Set the attachments of the message.
+    ///
+    /// Defaults to [`None`].
+    pub fn attachments(mut self, attachments: impl IntoIterator<Item = Attachment>) -> Self {
+        self.data.attachments = Some(attachments.into_iter().collect());
+        self
+    }
+
+    /// Set the message [`Component`]s of the reply.
+    ///
+    /// Defaults to [`None`].
+    pub fn components(mut self, components: impl IntoIterator<Item = Component>) -> Self {
+        self.data.components = Some(components.into_iter().collect());
+        self
+    }
+
+    /// Set this reply to clear the response's [`Component`]s. Only takes effect if this
+    /// reply is being used to modify an existing response.
+    ///
+    /// This is undone if components are set following this call.
+    pub fn clear_components(mut self) -> Self {
+        self.data.components = None;
+        self.clear_flags.insert(ClearFlags::COMPONENTS);
+        self
+    }
+
+    /// Set the message content of the reply.
+    ///
+    /// Defaults to [`None`].
+    pub fn content(mut self, content: impl Into<String>) -> Self {
+        self.data.content = Some(content.into());
+        self
+    }
+
+    /// Set this reply to clear the response's message content. Only takes effect if this
+    /// reply is being used to modify an existing response.
+    ///
+    /// This is undone if message content is set following this call.
+    pub fn clear_content(mut self) -> Self {
+        self.data.content = None;
+        self.clear_flags.insert(ClearFlags::CONTENT);
+        self
+    }
+
+    /// Set the [`Embed`]s of the reply.
+    ///
+    /// Defaults to an empty list.
+    pub fn embeds(mut self, embeds: impl IntoIterator<Item = Embed>) -> Self {
+        self.data.embeds = Some(embeds.into_iter().collect());
+        self
+    }
+
+    /// Set this reply to clear the response's [`Embed`]s. Only takes effect if this
+    /// reply is being used to modify an existing response.
+    pub fn clear_embeds(mut self) -> Self {
+        self.data.embeds = None;
+        self.clear_flags.insert(ClearFlags::EMBEDS);
+        self
+    }
+
+    /// Set the [`MessageFlags`].
+    ///
+    /// The only supported flags are [`EPHEMERAL`], [`SUPPRESS_EMBEDS`] and [`IS_COMPONENTS_V2`].
+    ///
+    /// Defaults to [`None`].
+    ///
+    /// [`EPHEMERAL`]: MessageFlags::EPHEMERAL
+    /// [`SUPPRESS_EMBEDS`]: MessageFlags::SUPPRESS_EMBEDS
+    /// [`IS_COMPONENTS_V2`]: MessageFlags::IS_COMPONENTS_V2
+    pub const fn flags(mut self, flags: MessageFlags) -> Self {
+        self.data.flags = Some(flags);
+        self
+    }
+
+    /// Set the reply's ephemeral status.
+    pub fn ephemeral(self, ephemeral: bool) -> Self {
+        if ephemeral {
+            self.add_flags(MessageFlags::EPHEMERAL)
+        } else {
+            self.remove_flags(MessageFlags::EPHEMERAL)
+        }
+    }
+
+    /// Set whether the response has text-to-speech enabled.
+    ///
+    /// Defaults to [`None`].
+    pub const fn tts(mut self, value: bool) -> Self {
+        self.data.tts = Some(value);
+        self
+    }
+
+    /// Set the poll of the reply.
+    pub fn poll(mut self, poll: Poll) -> Self {
+        self.data.poll = Some(poll);
+        self
+    }
+
+    /// Add a specific combination of flags to the reply's flags.
+    fn add_flags(self, flags: MessageFlags) -> Self {
+        let new_flags = match self.data.flags {
+            Some(old_flags) => old_flags | flags,
+            None => flags,
+        };
+        self.flags(new_flags)
+    }
+
+    /// Remove a specific combination of flags from the reply's flags.
+    fn remove_flags(mut self, flags: MessageFlags) -> Self {
+        self.data.flags = self.data.flags.map(|f| f.difference(flags));
+        self
+    }
+
+    /// Set the reply's suppress embeds flag.
+    fn suppress_embeds(self, suppress_embeds: bool) -> Self {
+        if suppress_embeds {
+            self.add_flags(MessageFlags::SUPPRESS_EMBEDS)
+        } else {
+            self.remove_flags(MessageFlags::SUPPRESS_EMBEDS)
+        }
+    }
+
+    /// Set the reply's components v2 flag.
+    fn components_v2(self, components_v2: bool) -> Self {
+        if components_v2 {
+            self.add_flags(MessageFlags::IS_COMPONENTS_V2)
+        } else {
+            self.remove_flags(MessageFlags::IS_COMPONENTS_V2)
+        }
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+    struct ClearFlags: u8 {
+        const MENTIONS   = 1 << 0;
+        const COMPONENTS = 1 << 1;
+        const CONTENT    = 1 << 2;
+        const EMBEDS     = 1 << 3;
+    }
+}
